18933549d7780d345b3781cb24d7dd5b2ef00f7e
12420 <1242004222@qq.com>
2021-03-27 15:21:41 +0800

    3.27

--- Model.py
+++ Model.py
@@ -1,12 +1,16 @@
 from torch import nn
 class Perception(nn.Module):
-    def __init__(self, in_dim, hid_dim, out_dim):
+    def __init__(self, in_dim, hid_dim1,hid_dim2,hid_dim3, out_dim):
         super(Perception,self).__init__()
         self.layer = nn.Sequential(
-            nn.Linear(in_dim,hid_dim),
+            nn.Linear(in_dim,hid_dim1),
             nn.Sigmoid(),
-            nn.Linear(hid_dim,out_dim),
-            nn.Sigmoid()
+            nn.Linear(hid_dim1,hid_dim2),
+            nn.Sigmoid(),
+            nn.Linear(hid_dim2, hid_dim3),
+            nn.Sigmoid(),
+            nn.Linear(hid_dim3, out_dim),
+            nn.Sigmoid(),
         )
     def forward(self,x):
         y=self.layer(x)
--- main.py
+++ main.py
@@ -5,30 +5,64 @@ from Model import Perception
 from torch import optim
 from tensorboardX import SummaryWriter
 import pandas as pd
-
-data = pd.read_csv("D:\city_temperature.csv")
-list=data.loc[:3000, ['AvgTemperature']].astype('float').values
+import numpy as np
+from matplotlib import pyplot as plt
+data = pd.read_csv("D:\city_temperature.csv")[:3000]
+# a=np.array(data['AvgTemperature'])
+# plt.hist(a, bins =  range(100))
+# plt.title("histogram")
+# plt.show()
+# plt.plot(range(3000),data['AvgTemperature'])
+# plt.show()
+# max=np.array([data.max().values[7]])
+# min=np.array([data.min().values[7]])
+max=np.array([84])
+min=np.array([43])
+list=data.loc[:3000, 'AvgTemperature'].astype('float').values
+for i in range(3000):
+    if(list[i]>84):list[i]=84
+    if(list[i]<43):list[i]=43
+list=(list-min)/(max-min)
 del data
 input=[]
-for i in range(10):
-    input.append(list[i][0])
-writer=SummaryWriter('logs/wendu')
-model=Perception(10,3,1)
-optimizer=optim.SGD(params=model.parameters(),lr=0.02)
-criterion = nn.CrossEntropyLoss()
-
 for i in range(1000):
-    input.pop(0)
-    input.append(list[i+10][0])
-    input=torch.Tensor(input).float()
-    label=list[i+11]
-    label=torch.Tensor(label).float()
+    input.append(list[i:i+10])
+label=list[10:1010]
+writer=SummaryWriter('logs/wendu')
+model=Perception(10,100,20,3,1)
+optimizer=optim.SGD(params=model.parameters(),lr=2)
+MSE = nn.MSELoss()
+
+for i in range(2000):
+    input=torch.tensor(input).float()
+    label=torch.tensor(label).reshape([1000,1]).float()
     output=model(input)
-    loss=criterion(output,label)
+    loss=MSE(output,label)
     writer.add_scalar('loss/total_loss',loss.item(),i)
-    if i%50==0:
-        print(loss.item())
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+    if i%50==0:
+        print(loss.item())
+    if i==1999:
+        label.reshape([1000])
+        output.reshape([1000])
+        label=label.detach().numpy()
+        output=output.detach().numpy()
+        x=range(1000)
+        plt.plot(x,label,"g")
+        plt.plot(x,output,"r")
+        plt.show()
 
+test_x=[]
+for i in range(1000,1100):
+    test_x.append(list[i:i+10])
+test_label=list[1010:1110]
+test_x=torch.tensor(test_x).float()
+test_y=model(test_x)
+test_y.reshape([100])
+test_y=test_y.detach().numpy()
+x=range(100)
+plt.plot(x,test_label,"g")
+plt.plot(x,test_y,"r")
+plt.show()
